634897b5c471d71f6dea0986a9cc670e180b6d68
Masia Antoine <pro.antoine.masia@gmail.com>
2023-10-16 21:31:44 +0200

    conversion en objet

--- lib-loading-masia-v1.1/app.js
+++ lib-loading-masia-v1.1/app.js
@@ -1,73 +1,50 @@
-const linkElement = document.createElement('link');
-const loadingBackground = document.createElement('div');
-const loadingLogo = document.createElement('img');
-
-function createBackground() {
-    // Créez votre élément
-    loadingBackground.id = 'loading-background';
-
-    // Ajoutez à la page
-    let body = document.body;
-    body.insertBefore(loadingBackground, body.firstChild);
-}
-
-function createLogo(path, borderRadius, animation) {
-    // Créez votre élément loadingLogo
-
-    loadingLogo.id = 'loading-logo';
-
-    // Définissez la source de loadingLogo avec l'URL de l'icône du JSON
-    loadingLogo.src = path;
-
-    // Ajoutez loadingLogo à la page
-    let body = document.body;
-    body.insertBefore(loadingLogo, body.firstChild);
-
-    // Vérifiez la valeur de borderRadius
-    if (borderRadius === true) {
-        // Si borderRadius est true, ajoutez la classe border-radius au logo
-        loadingLogo.classList.add('border-radius');
+class LoadingScreen {
+    constructor() {
+        this.linkElement = document.createElement('link');
+        this.loadingBackground = document.createElement('div');
+        this.loadingLogo = document.createElement('img');
     }
 
-    loadingLogo.classList.add('animation-' + animation);
-}
+    _createBackground() {
+        this.loadingBackground.id = 'loading-background';
+        let body = document.body;
+        body.insertBefore(this.loadingBackground, body.firstChild);
+    }
 
-function addCSS(pathLib) {
-    // Créez un élément <link> pour la feuille de style
+    _createLogo(path, borderRadius, animation) {
+        this.loadingLogo.id = 'loading-logo';
+        this.loadingLogo.src = path;
+        let body = document.body;
+        body.insertBefore(this.loadingLogo, body.firstChild);
 
-    linkElement.rel = 'stylesheet';
-    linkElement.href = pathLib + '/style.css';
+        if (borderRadius === true) {
+            this.loadingLogo.classList.add('border-radius');
+        }
 
-    // Récupérez la balise <head>
-    let head = document.head || document.getElementsByTagName('head')[0];
+        this.loadingLogo.classList.add('animation-' + animation);
+    }
 
-    // Ajoutez l'élément <link> à la fin de la balise <head>
-    head.appendChild(linkElement);
-}
+    _addCSS(pathLib) {
+        this.linkElement.rel = 'stylesheet';
+        this.linkElement.href = pathLib + '/style.css';
+        let head = document.head || document.getElementsByTagName('head')[0];
+        head.appendChild(this.linkElement);
+    }
 
-// La fonction mère du chargement
-/**
- * chargement de votre loader
- * @param {*} config objet de configuration devant contenir icon.path, icon.borderRadius, icon.animation et pathLib
- */
-function createLoading(config) {
-    const path = (config && config.icon && config.icon.path) ? config.icon.path : "https://masia-antoine.fr/profil/resources/images/logo.png";
-    const borderRadius = (config && config.icon && config.icon.borderRadius) ? config.icon.borderRadius : false;
-    const animation = (config && config.icon && config.icon.animation) ? config.icon.animation : "rotate";
-    const pathLib = (config && config.pathLib) ? config.pathLib : "/lib-loading-masia-v1.0"
+    createLoading(config) {
+        const path = (config && config.icon && config.icon.path) ? config.icon.path : "https://masia-antoine.fr/profil/resources/images/logo.png";
+        const borderRadius = (config && config.icon && config.icon.borderRadius) ? config.icon.borderRadius : false;
+        const animation = (config && config.icon && config.icon.animation) ? config.icon.animation : "rotate";
+        const pathLib = (config && config.pathLib) ? config.pathLib : "/lib-loading-masia-v1.0";
 
-    addCSS(pathLib);
+        this._addCSS(pathLib);
+        this._createLogo(path, borderRadius, animation);
+        this._createBackground();
+    }
 
-    createLogo(path, borderRadius, animation);
-
-    createBackground();
-}
-
-/**
- * Stop de votre loader
- */
-function stopLoading() {
-    linkElement.remove()
-    loadingBackground.remove()
-    loadingLogo.remove()
+    stopLoading() {
+        this.linkElement.remove()
+        this.loadingBackground.remove()
+        this.loadingLogo.remove()
+    }
 }
